b2902d8f63655da9a3069c46033bdc3750805859
Joseph G. Varilla <jvarilla981@gmail.com>
2018-11-16 16:53:47 -0600

    add jwt

--- core/config.js
+++ core/config.js
@@ -7,9 +7,9 @@ exports.default = {
   server: {
     port: process.env.PORT || 3000,
     env: process.env.NODE_ENV || 'development',
-    jwt: process.env.JWT || 'stubJWT'
+    jwt: process.env.JWT || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViZWQ4ZmIyODU0MjQxMTYwMDJhODJiMyIsInVzZXJuYW1lIjoianZhcmlsbGE5ODEiLCJleHAiOjE1NDc0Nzk0NzQsImlhdCI6MTU0MjI5NTQ3NH0.lDzxCuLrMcl_1D3G6f1XAdH021eBG6gRK7-R-V6pB-c'//'stubJWT'
   },
   api: {
-	  base_url: process.env.BASE_URL || 'https://conduit.productionready.io/api' //https://powerful-lake-14918.herokuapp.com/api'
+	  base_url: process.env.BASE_URL || '//https://powerful-lake-14918.herokuapp.com/api'
   }
 };
