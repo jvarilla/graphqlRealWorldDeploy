e00bbbc6d9f4523d56ac0308c13cfc1c72d3f1fe
Joseph G. Varilla <jvarilla981@gmail.com>
2018-11-16 14:23:58 -0600

    Changed url in doc file then built

--- core/config.js
+++ core/config.js
@@ -7,10 +7,9 @@ exports.default = {
   server: {
     port: process.env.PORT || 3000,
     env: process.env.NODE_ENV || 'development',
-    jwt: process.env.JWT || 'conduit' //'stubJWT'
+    jwt: process.env.JWT || 'stubJWT'
   },
   api: {
-      base_url: process.env.BASE_URL || 'https://powerful-lake-14918.herokuapp.com/api'
-      //base_url: process.env.BASE_URL || 'https://conduit.productionready.io/api'
+    base_url: process.env.BASE_URL || 'https://powerful-lake-14918.herokuapp.com/api'
   }
 };
