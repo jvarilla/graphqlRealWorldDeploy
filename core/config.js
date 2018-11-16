391646d2112599f796b83d58c3d311f7fd0e9d83
Joseph G. Varilla <jvarilla981@gmail.com>
2018-11-16 11:53:28 -0600

    swith to development

--- core/config.js
+++ core/config.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = {
   server: {
     port: process.env.PORT || 3000,
-    env: process.env.NODE_ENV || 'production',//'development',
+    env: process.env.NODE_ENV || 'development',
     jwt: process.env.JWT || 'stubJWT'
   },
   api: {
