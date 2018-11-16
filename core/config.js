924b7c0a8bf97b97d4de9e3f8b6854bfcb771d11
Joseph G. Varilla <jvarilla981@gmail.com>
2018-11-16 12:29:34 -0600

    Change JWT

--- core/config.js
+++ core/config.js
@@ -7,7 +7,7 @@ exports.default = {
   server: {
     port: process.env.PORT || 3000,
     env: process.env.NODE_ENV || 'development',
-    jwt: process.env.JWT || 'stubJWT'
+    jwt: process.env.JWT || 'conduit' //'stubJWT'
   },
   api: {
       base_url: process.env.BASE_URL || 'https://powerful-lake-14918.herokuapp.com/api'
